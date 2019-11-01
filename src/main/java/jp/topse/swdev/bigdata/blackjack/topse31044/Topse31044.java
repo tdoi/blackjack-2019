4c43bf830bf705d062f65e61c4c7f8c5c7a4fb85
topse31044 <topse31044@tc08.cb.ecloud.nii.ac.jp>
2019-11-01 21:20:25 +0900

    コミットテスト

--- src/main/java/jp/topse/swdev/bigdata/blackjack/topse31044/Topse31044.java
+++ src/main/java/jp/topse/swdev/bigdata/blackjack/topse31044/Topse31044.java
@@ -1,7 +1,7 @@
 package jp.topse.swdev.bigdata.blackjack.topse31044;
 
 public class Topse31044 {
-	// テスト
+	// テスト2
 	private static final String ARFF_PATH= "models/topse31044.arff";
 	private static final String ARFF_PATH_out= "models/topse31044.arff";
 
